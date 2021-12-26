65a62296675bdca955b2830e43ececbc843d1857
WeiCherng <weicherng0719@gmail.com>
2021-12-26 21:29:08 +0800

    Added Scoring System

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+const cleared = () => {
+  winScore.innerHTML = 0;
+  loseScore.innerHTML = 0;
+  win = 0;
+  lose = 0;
+}
+const updateScore = () => {
+  winScore.innerHTML = win;
+  loseScore.innerHTML = lose;
+}
+
 const game = (playerSelection) => {
-    // const playerSelection = playerPlay();
     const computerSelection = computerPlay();
     return (playRound(playerSelection, computerSelection));
 };
@@ -10,22 +20,28 @@ const playRound = (playerSelection, computerSelection) => {
   }
   if (playerSelection == "Paper") {
     if (computerSelection == "Rock") {
-      return "You Win! Paper beats Rock";
+      win++;
+      return ("You Win! Paper beats Rock");
     } else if (computerSelection == "Scissor") {
+      lose++;
       return "You Lose! Scissors beats Paper";
     }
   }
   if (playerSelection == "Rock") {
     if (computerSelection == "Paper") {
+      lose++;
       return "You Lose! Paper beats Rock";
     } else if (computerSelection == "Scissor") {
+      win++;
       return "You Win! Rock beats Scissors";
     }
   }
   if (playerSelection == "Scissor") {
     if (computerSelection == "Paper") {
+      win++;
       return "You Win! Scissors beats Paper";
     } else if (computerSelection == "Rock") {
+      lose++;
       return "You Lose! Rock beats Scissors";
     }
   }
@@ -64,18 +80,30 @@ const paper = document.querySelector(".paper");
 const rock = document.querySelector(".rock");
 const scissor = document.querySelector(".scissor");
 const output = document.querySelector("#output");
+const clear = document.querySelector(".clear");
+
+let winScore = document.querySelector(".win");
+let loseScore = document.querySelector(".lose");
+let win = 0;
+let lose = 0;
 
 paper.addEventListener('click', function(e) {
   const string = game('Paper');
   output.innerHTML = string
+  updateScore()
 })
 rock.addEventListener('click', function(e) {
   const string = game('Rock')
   output.innerHTML = string
+  updateScore()
 })
 scissor.addEventListener('click', function(e) {
   const string = game('Scissor')
   output.innerHTML = string
+  updateScore()
+})
+clear.addEventListener('click', function(e) {
+  cleared();
 })
 
 
